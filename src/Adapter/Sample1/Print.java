581df66af8c1f4130fd2b0378d89c3c03f40100d
salingers <salingersms@msn.com>
2016-08-02 12:01:46 +0800

    0802

--- src/Adapter/Sample1/Print.java
+++
@@ -1,8 +0,0 @@
-package Adapter.Sample1;
-
-public interface Print
-{
-	public abstract void printWeak();
-
-	public abstract void printStrong();
-}
--- src/Adapter/Sample2/Print.java
+++
@@ -1,8 +0,0 @@
-package Adapter.Sample2;
-
-public abstract class Print
-{
-	public abstract void printWeak();
-
-	public abstract void printStrong();
-}
